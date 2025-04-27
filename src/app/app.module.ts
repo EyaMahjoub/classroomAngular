4a3d9b02c117d2b4a3ccb1c1ea3275e46b86733d
achwak <belliliachwak@gmail.com>
2025-04-27 10:29:52 +0100

    exemple 1 de api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { EtudiantComponent } from './etudiant/etudiant.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -22,7 +22,8 @@ import { EnseignantComponent } from './enseignant/enseignant.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/enseignant.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EnseignantService {
+  constructor(private http: HttpClient) { }
+  getlistenseignant(): Observable<any> {
+    // Utilisation de l'URL complète de l'API
+    return this.http.get('http://127.0.0.1:8000/api/enginant'); // Ne pas oublier de spécifier l'URL complète
+  }
+}
